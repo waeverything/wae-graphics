3641bd74deac591db8951bf54904da225f7c9d9a
Fabian Mäkilä <44451855+FaberoM@users.noreply.github.com>
2019-12-22 20:54:47 +0200

    Use replicants on textpanels

--- dashboard/intermission/js/page.js
+++ dashboard/intermission/js/page.js
@@ -4,10 +4,7 @@ const submit = document.querySelector('#submit');
 const textReplicant = nodecg.Replicant('intermission-text');
 
 textReplicant.on('change', (newValue, oldValue) => {
-  // The value of the Replicant has changed somewhere in NodeCG,
-  // this could be another dashboard panel, a server initiated change,
-  // or the doing of another user accessing your dashboard panel.
-  nameInput.value = newValue;
+  textInput.value = newValue;
 });
 
 submit.onclick = () => {
