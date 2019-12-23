5e1107f41c40e7d65a647d540966591bb7b89d66
Fabian Mäkilä <44451855+FaberoM@users.noreply.github.com>
2019-12-23 18:18:09 +0200

    Use svg background instead of jpg.

--- graphics/text-panels/js/page.js
+++ graphics/text-panels/js/page.js
@@ -1,12 +1,12 @@
 //Show panel for 10 seconds
 function showPanel(ID) {
-  let element = document.getElementById(ID);
+	let element = document.getElementById(ID);
 
-  element.classList.add("show");
+	element.classList.add("show");
 
-  setTimeout(function() {
-    element.classList.remove("show");
-  }, 10000);
+	setTimeout(function() {
+		element.classList.remove("show");
+	}, 10000);
 }
 
 let bottomContent = document.getElementById("bottomContent");
@@ -14,36 +14,36 @@ let bottomContent = document.getElementById("bottomContent");
 //Bottom panel
 nodecg.listenFor('bottomMessage', (value, ack) => {
 
-  bottomContent.innerHTML = "";
+	bottomContent.innerHTML = "";
 
-  let names = value.split(";");
-  for (var i = 0; i < names.length; ++i) {
-    let nameBox = document.createElement("H3");
-    nameBox.innerHTML = names[i];
-    bottomContent.appendChild(nameBox);
-  }
+	let names = value.split(";");
+	for (var i = 0; i < names.length; ++i) {
+		let nameBox = document.createElement("H3");
+		nameBox.innerHTML = names[i];
+		bottomContent.appendChild(nameBox);
+	}
 
-  showPanel("bottomPanel");
+	showPanel("bottomPanel");
 });
 
 //Left panel
 nodecg.listenFor('leftMessage', (value, ack) => {
-  let textfields = value.split("§");
-  document.getElementById("leftUpper").innerHTML = textfields[0];
-  if (textfields[1] != null) {
-    document.getElementById("leftLower").innerHTML = textfields[1];
-  }
+	let textfields = value.split("§");
+	document.getElementById("leftUpper").innerHTML = textfields[0];
+	if (textfields[1] != null) {
+		document.getElementById("leftLower").innerHTML = textfields[1];
+	}
 
-  showPanel("leftPanel");
+	showPanel("leftPanel");
 });
 
 //Right panel
 nodecg.listenFor('rightMessage', (value, ack) => {
-  let textfields = value.split("§");
-  document.getElementById("rightUpper").innerHTML = textfields[0];
-  if (textfields[1] != null) {
-    document.getElementById("rightLower").innerHTML = textfields[1];
-  }
+	let textfields = value.split("§");
+	document.getElementById("rightUpper").innerHTML = textfields[0];
+	if (textfields[1] != null) {
+		document.getElementById("rightLower").innerHTML = textfields[1];
+	}
 
-  showPanel("rightPanel");
+	showPanel("rightPanel");
 });
