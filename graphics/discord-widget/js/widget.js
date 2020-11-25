982c86e400d0304b7fff42cfe99d5cd7c670d679
Teatteri Perikunta <teatteriperikunta@Teatteri-MacBook-Air.local>
2020-11-25 10:41:23 +0200

    Update discord widget

--- graphics/discord-widget/js/widget.js
+++ graphics/discord-widget/js/widget.js
@@ -1,38 +1,40 @@
-//JSON URL
-let url = 'https://discordapp.com/api/guilds/95873997861834752/widget.json';
+// This script makes use of Discord's Server Widget feature which has a integrated json api.
+// For this to work, make sure that you have enabled Server Widget for your Discord server.
 
-//Time online members are shown on screen
-let time = 7000;
 
-//Wait interval before showing online members again
-let interval = 80000;
+// Put your guild ID here
+const guildID = '95873997861834752';
 
-//Delay before the code will be executed for the first time
-let delay = 3000;
+// Delay in seconds before the code will be executed for the first time
+const initialDelay = 3;
 
-let elem = document.getElementById("memberCount");
-let div = document.getElementById("onlineCounter").classList;
+// Time in seconds of how long the member counter is shown on the screen
+const displayTime = 7;
 
-//Start script
-setTimeout(update, delay);
+// Time in seconds of how long should we wait before showing the counter again
+const fetchInterval = 80;
 
-//Run script again every x seconds
-window.setInterval(function() {
-	update();
-}, interval);
 
-//Update and show online members
-function update() {
-	fetch(url)
-		.then(res => res.json())
-		.then((out) => {
-			elem.innerHTML = out.members.length;
-			div.add("show");
-			setTimeout(function() {
-				div.remove("show");
-			}, time);
-		})
-		.catch(err => {
-			throw err
-		});
-};
+const div = document.getElementById("onlineCounter").classList;
+const elem = document.getElementById("memberCount");
+
+// Fetch and show the member counter
+function showOnlineMembers() {
+  fetch('https://discordapp.com/api/guilds/' + guildID + '/widget.json')
+    .then(res => res.json())
+    .then((out) => {
+      elem.innerHTML = out.presence_count;
+      div.add("show");
+      setTimeout(function () {
+        div.remove("show");
+      }, displayTime * 1000);
+    })
+    .catch(err => {
+      throw err;
+    });
+}
+
+setTimeout(function () {
+  showOnlineMembers();
+  setInterval(showOnlineMembers, fetchInterval * 1000);
+}, initialDelay * 1000);
